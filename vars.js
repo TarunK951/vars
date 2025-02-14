1abe621ed27cdc58ac3ec27f4f61c3fa42c87b73
Tarun <119518924+TarunK951@users.noreply.github.com>
2025-02-14 19:07:05 +0530

    main

---
+++ vars.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Are you positive?",
+    "Pookie please...",
+    "Just think about it!",
+    "If you say no, I will be really sad...",
+    "I will be very sad...",
+    "I will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+function no() {
+    const noButton = document.querySelector('.noBtn');
+    const yesButton = document.querySelector('.yesBtn');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function yes() {
+    window.location.href = "./yes.html";
+}
